b4371e4ee898f712e0a2b020ce0bc2150544ee02
Reza Nosratpour <93834390+Rezanosratpour@users.noreply.github.com>
2023-02-14 02:21:33 +0330

    Update PFA_.py

--- PFA_.py
+++ PFA_.py
@@ -1,19 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec  6 08:10:29 2021
 
-@author: reza
-"""
-
-import numpy
+import numpy 
+import numpy as np
 import math 
 import random
 from math import *
+import colorama
+from colorama import Fore
 
-
-class PFA_model():
+class model():
     
-    def __init__(self ,   fitness_function  , num_of_parameters   , initialize_iteration  , PFA_iteration ,alpha =1  , beta=1 , converging_threshold = 0.001):
+    def __init__(self ,   fitness_function  , num_of_parameters   , initialize_iteration  , PFA_iteration , alpha =1  , beta=1 , converging_threshold = 0.001):
         
         self.fitness_function = fitness_function
         self.num_of_param     = num_of_parameters
@@ -34,20 +30,22 @@ class PFA_model():
         mem=0
         
         """  --  initialize -- """
-        it=0
+        it=0 
+        self.best_fit = 9*10**100
         while it<self.IT or mem<2 :
             it+=1
             sol =numpy.random.normal(-5, 5, self.num_of_param) 
-            fitness = self.fitness_function(sol)
-            if fitness<self.best_fit :
-                self.Conv_curve.append(fitness)
+            self.fitness = self.fitness_function(sol)
+            if self.fitness < self.best_fit :
+                self.Conv_curve.append(self.fitness)
                 mem+=1
-                self.best_fit=fitness
+                self.best_fit=self.fitness
                 self.best_sol=sol
                 Dict[str(mem)]=sol
-                Loss[str(mem)]=fitness
-                print("Solution :",sol)
-                print("fitness = ",fitness)
+                Loss[str(mem)]=self.fitness
+                # print("Solution :",sol)
+                # print("fitness = ",self.fitness)
+                
         self.num_of_mem = len(Dict)
             
         self.best_sol=Dict[str(self.num_of_mem)]
@@ -86,21 +84,22 @@ class PFA_model():
                 sol = Dict[str(mem)]
                 dpos= position[str(it+1)]-sol
                 sol =  sol+R1*dif+R2*dpos+epsilon
-                fitness=self.fitness_function(sol )
-                if fitness<Loss[str(mem)]:
+                self.fitness=self.fitness_function(sol )
+                if self.fitness<Loss[str(mem)]:
                     print(it,"of",self.IT2)
                     Dict[str(mem)] = sol
-                    Loss[str(mem)] = fitness
-                if fitness<self.best_fit:
+                    Loss[str(mem)] = self.fitness
+                
+                if self.fitness< self.best_fit :
                     self.best_sol = sol
-                    self.best_fit = fitness
-                    self.Conv_curve.append(fitness)
+                    self.best_fit = self.fitness
+                    self.Conv_curve.append(self.fitness)
                     
                     print("Solution :",sol)
-                    print("fitness = ",fitness)
+                    print("fitness = ",self.fitness)
                 position[str(it)] = self.best_sol
             position[str(it+1)] = self.best_sol
         
         if abs(self.Conv_curve[-2]-self.Conv_curve[-1]) > self.conv :
-            print( '  warning :  Converging threshold is not satisfied ' )
-
+            
+            print( Fore.RED + '  warning :  Converging threshold is not satisfied ' )
